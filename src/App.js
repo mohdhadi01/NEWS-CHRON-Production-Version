3432c71bf612fcf19361a08e9faee625b22aca67
mohdhadi01 <hadi.88923@gmail.com>
2023-12-30 01:23:36 +0530

    #update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import NewsWebApp from './NEWS/NewsWebApp';
 
 
 function App() {
-   const apiKey =process.env.REACT_APP_NEWS_API
+  //  const apiKey =process.env.REACT_APP_NEWS_API
+  const apiKey ="pub_347817b7acdf7abf975217c7bd7e9ee528a91"
    
   return (
     <div>
