a3d5605aebf75836dcac593723851d209182d9b7
mohdhadi01 <hadi.88923@gmail.com>
2023-12-30 01:21:54 +0530

    #created env package to handle API leak

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import NewsWebApp from './NEWS/NewsWebApp';
 
 
 function App() {
-  //  const apiKey =process.env.REACT_APP_NEWS_API
-  const apiKey ="pub_347817b7acdf7abf975217c7bd7e9ee528a91"
+   const apiKey =process.env.REACT_APP_NEWS_API
    
   return (
     <div>
